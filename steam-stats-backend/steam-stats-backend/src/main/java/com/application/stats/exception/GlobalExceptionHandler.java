b7b74704a11b6d0fd53164aac9e13fd639d9ae3d
Matheus Gilla Sestare <mthsestare@hotmail.com>
2026-06-15 15:18:59 -0300

    add exception handlers

--- steam-stats-backend/steam-stats-backend/src/main/java/com/application/stats/exception/GlobalExceptionHandler.java
+++ steam-stats-backend/steam-stats-backend/src/main/java/com/application/stats/exception/GlobalExceptionHandler.java
@@ -1,21 +1,41 @@
 package com.application.stats.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.Map;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NotFound.class)
     public ResponseEntity<Map<String, String>> handlePlayerNotFoundException(NotFound ex) {
-        return ResponseEntity.status(404).body(Map.of("error", ex.getMessage()));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", ex.getMessage()));
     }
 
     @ExceptionHandler(PrivateProfile.class)
     public ResponseEntity<Map<String, String>> handlePrivateProfileException(PrivateProfile ex) {
-        return ResponseEntity.status(500).body(Map.of("error", ex.getMessage()));
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", ex.getMessage()));
+    }
+
+    @ExceptionHandler(HttpServerErrorException.InternalServerError.class)
+    public ResponseEntity<Map<String, String>> handleRuntimeException(HttpServerErrorException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "An unexpected error occurred: " + ex.getMessage()));
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<Map<String, String>> handleHttpResponseException(NoResourceFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", ex.getMessage()));
+    }
+
+    @ExceptionHandler(HttpClientErrorException.BadRequest.class)
+    public ResponseEntity<Map<String, String>> handleBadRequestException() {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", "400: Bad request"));
     }
 }
